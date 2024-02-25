e265d19acb396f937b8ed98fd801347ffcb98c0f
DESKTOP-DBEI4M5\MarBog <marchuk5982@gmail.com>
2024-02-25 17:53:44 +0200

    first lab

---
+++ Lab1/agent/src/file_datasource.py
@@ -0,0 +1,43 @@
+from csv import reader
+from datetime import datetime
+
+from domain.parking import Parking
+from domain.aggregated_data import AggregatedData
+from domain.accelerometer import Accelerometer
+from domain.gps import Gps
+
+class FileDatasource:
+    def __init__(self, accelerometer_filename: str, gps_filename: str, parking_filename: str) -> None:
+        self.accelerometer_filename = accelerometer_filename
+        self.gps_filename = gps_filename
+        self.parking_filename = parking_filename
+        self.cache_data = {}
+
+    def read(self) -> AggregatedData:
+        try:
+            accelerometer_data = next(reader(self.cache_data["accelerometer"]))
+            gps_data = next(reader(self.cache_data["gps"]))
+            parking_data = next(reader(self.cache_data["parking"]))
+
+            x, y, z = map(int, accelerometer_data)
+            longitude, latitude = map(float, gps_data)
+            empty_count = int(parking_data[0])
+            return AggregatedData(accelerometer=Accelerometer(x=x, y=y, z=z),
+                                  gps=Gps(longitude=longitude, latitude=latitude),
+                                  parking=Parking(empty_count, Gps(longitude=longitude, latitude=latitude)),
+                                  time=datetime.now())
+        except StopIteration:
+            self.stopReading()
+            StopIteration("No data")
+        except ValueError as error:
+            ValueError(error)
+
+    def startReading(self, *args, **kwargs):
+        self.cache_data["accelerometer"] = open(self.accelerometer_filename, 'r')
+        self.cache_data["gps"] = open(self.gps_filename, 'r')
+        self.cache_data["parking"] = open(self.parking_filename, 'r')
+
+    def stopReading(self, *args, **kwargs):
+        self.cache_data["accelerometer"].close()
+        self.cache_data["gps"].close()
+        self.cache_data["parking"].close()
